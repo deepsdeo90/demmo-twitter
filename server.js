13be06c1a30e2a48a94738b93a1839f028099ad7
deepsdeo90 <deepalideore123@gmail.com>
2018-05-16 17:01:34 -0700

    gui changes

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/demo-twitter/'));
-/* consumer_key: 'EMU5G64RqPfrLnf6ebp3RsA2S',
- consumer_secret: 'aWKp29GUseDyqGynDFcC5RArx6SPeU7ulvTp95GMmXLZYK9Cfg',
- access_token: '453947367-8lJmZPMR69GEgNgksrDIUDG7GjKA1XDvROTNB42c',
- access_token_secret: 'MaUb6uLojEMMjRMwSjDkNlSU8KJwBUwQ0Y14Uw2XLl2fo'*/
-
 const client = new Twitter({
  consumer_key: process.env.CONSUMER_KEY_TWITTER,
  consumer_secret: process.env.CONSUMER_SECRET,
@@ -19,15 +14,6 @@ const client = new Twitter({
  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-
-/*
-const client = new Twitter({
-  consumer_key: 'EMU5G64RqPfrLnf6ebp3RsA2S',
- consumer_secret: 'aWKp29GUseDyqGynDFcC5RArx6SPeU7ulvTp95GMmXLZYK9Cfg',
- access_token: '453947367-8lJmZPMR69GEgNgksrDIUDG7GjKA1XDvROTNB42c',
- access_token_secret: 'MaUb6uLojEMMjRMwSjDkNlSU8KJwBUwQ0Y14Uw2XLl2fo'
-});
-*/
 app.use(require('cors')());
 app.use(require('body-parser').json());
 
@@ -39,11 +25,7 @@ res.sendFile(path.join(__dirname+'/dist/demo-twitter/index.html'));
 });
 */
 app.get('/api/searchTweets', (req, res) => {
- /* console.log("consumerkey "+process.env.CONSUMER_KEY_TWITTER);
-console.log(process.env.CONSUMER_SECRET);
-console.log(process.env.ACCESS_TOKEN);
-console.log(process.env.ACCESS_TOKEN_SECRET);
-*/
+
     const params = { tweet_mode: 'extended', count: 200,result_type: 'recent' };
    // console.log(req.query.search);
     if (req.query.search) {
